1ecf536c332976ea08c46d0cafffc9a4ca535114
AbdallahAttili <abdallahattili@gmail.com>
2024-05-25 20:30:56 +0300

    starting the application using the Spring initializr

---
+++ main-application/src/main/java/com/task/demo/movie_database_api/MovieDatabaseApiApplication.java
@@ -0,0 +1,13 @@
+package com.task.demo.movie_database_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDatabaseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieDatabaseApiApplication.class, args);
+	}
+
+}
